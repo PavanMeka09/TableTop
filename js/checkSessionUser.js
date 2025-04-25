9fec5deda706508402d2235ed571beae4a2cdf14
PavanMeka09 <Mekapavan99@gmail.com>
2025-04-25 10:40:32 +0530

    update checkSession

--- js/checkSessionUser.js
+++ js/checkSessionUser.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded', () => {
   try {
-      const res = await fetch('http://localhost/tabletop/backend/auth.php', {
+      const res = fetch('http://localhost/tabletop/backend/auth.php', {
           method: 'POST',
           headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
           body: new URLSearchParams({ action: 'check_session' })
       });
-      const data = await res.json();
+      const data = res.json();
       if (data.role === 'admin') {
           window.location.href = '/admin/dashboard.html';
         } else if(!data.logged_in){
